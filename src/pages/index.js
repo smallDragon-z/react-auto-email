1c255207ed3daaea07f056e509f4ccf9394cc8f7
zhangXiaoLong <744823391@qq.com>
2024-01-18 10:37:59 +0800

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,79 @@
-import React from 'react';
-import { Button } from 'antd';
+import ReactECharts from 'echarts-for-react';
+import { Button, Table } from 'antd';
+import { useState } from 'react';
 
-export default function Home() {
+const Home = ({ data }) => {
+  const [number, setNumber] = useState(0);
+  const setYData = (data) => {
+    return data.map((item) => Number(item.year));
+  };
+  const setXData = (data) => {
+    return data.map((item) => item.title);
+  };
+  const xData = setXData(data);
+  const yData = setYData(data);
+
+  const option = {
+    xAxis: {
+      type: 'category',
+      data: xData,
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        data: yData,
+        type: 'line',
+      },
+    ],
+  };
+
+  const columns = [
+    {
+      title: '年份',
+      dataIndex: 'year',
+      key: 'name',
+    },
+    {
+      title: '标题',
+      dataIndex: 'title',
+      key: 'age',
+    },
+    {
+      title: '描述',
+      dataIndex: 'desc',
+      key: 'address',
+    },
+  ];
   return (
-    <>
-      <Button type={'primary'}>
-        <span>Button</span>
+    <div className="App">
+      <Button type="primary" onClick={() => setNumber((v) => v - 1)}>
+        -
       </Button>
-    </>
+      <div>{number}</div>
+      <Button type="primary" onClick={() => setNumber((v) => v + 1)}>
+        +
+      </Button>
+      <ReactECharts option={option} />
+      <Table dataSource={data} columns={columns} />
+    </div>
   );
+};
+export async function getServerSideProps(context) {
+  const res = await fetch(`https://api.oioweb.cn/api/common/history`);
+  const data = await res.json();
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      data: data.result || [],
+    }, // will be passed to the page component as props
+  };
 }
+export default Home;
