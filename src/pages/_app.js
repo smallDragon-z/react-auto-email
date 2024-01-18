1a727fec7abec879947f0750ea700c102139c5a3
zhangXiaoLong <744823391@qq.com>
2024-01-18 09:58:29 +0800

    将react和react-dom的引入放到_app.js下

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { ConfigProvider } from 'antd';
-
+import '../../public/react-min-package/react.min';
+import '../../public/react-min-package/react-dom.min.js';
 import theme from '../theme/themeConfig';
 
 const App = ({ Component, pageProps }) => (
-    <ConfigProvider theme={theme}>
-      <Component {...pageProps} />
-    </ConfigProvider>
+  <ConfigProvider theme={theme}>
+    <Component {...pageProps} />
+  </ConfigProvider>
 );
 
 export default App;
